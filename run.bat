23bb69868d3ea90d5e59791116c358f534f256b8
Smooth E <smoothie@disroot.org>
2023-10-27 14:30:01 +0300

    Исправлены опечатки

--- run.bat
+++ run.bat
@@ -1,11 +1,15 @@
 @echo off
 
 python --version || goto no_python
-pip --version ||  goto no_python
+pip --version ||  goto no_pip
 goto python_present
 
 :no_python
-echo "На вашем компьютере не установлен Python  или pip"
+echo "На вашем компьютере не установлен Python!"
+goto eof
+
+:no_pip
+echo "На вашем компьютере не установлен pip!"
 goto eof
 
 :python_present
